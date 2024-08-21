18279c5a4dc91a254441f1414e33815dbe4af0cf
jamesread <contact@jread.com>
2024-08-21 22:02:37 +0100

    Revert "Sort alerts by severity"
    This reverts commit 132dd150e2b48af5f93fe9f14b4f7316d6db3859.

--- webui/js/main.js
+++ webui/js/main.js
@@ -5,21 +5,9 @@ export default function main () {
   setInterval(fetchAlertList, 30000)
 }
 
-function clearAlertList (clazz) {
-  let alertList = document.getElementById(clazz)
-
-  alertList.innerHTML = ''
-
-  return alertList
-}
-
 function fetchAlertList () {
-  clearAlertList('default')
-  clearAlertList('critical')
-  clearAlertList('info')
-  clearAlertList('warning')
-
-  const defaultAlertList = document.getElementById('default')
+  const alertList = document.getElementById('alert-list')
+  alertList.innerHTML = ''
 
   fetch('/alert_list')
     .then(response => response.json())
@@ -27,28 +15,18 @@ function fetchAlertList () {
       const alerts = res.Alerts
 
       for (const alert of Object.keys(alerts)) {
-        let alertList = document.getElementById(alerts[alert].Labels.severity)
-
-        if (alertList === null) {
-          alertList = defaultAlertList
-        }
-
         alertList.appendChild(renderAlert(alerts[alert]))
       }
 
-      updateLastUpdated(res)
+      if (res.LastUpdated > 0) {
+        const lastUpdatedDate = new Date(res.LastUpdated * 1000)
+        const deltaLastUpdated = lastUpdatedDate - new Date()
+        const formatter = new Intl.RelativeTimeFormat()
+
+        document.getElementById('last-updated').textContent = formatter.format(Math.floor(deltaLastUpdated / 1000), 'seconds')
+        document.getElementById('last-updated').title = lastUpdatedDate.toLocaleString()
+      }
     })
-}
-
-function updateLastUpdated (res) {
-  if (res.LastUpdated > 0) {
-    const lastUpdatedDate = new Date(res.LastUpdated * 1000)
-    const deltaLastUpdated = lastUpdatedDate - new Date()
-    const formatter = new Intl.RelativeTimeFormat()
-
-    document.getElementById('last-updated').textContent = formatter.format(Math.floor(deltaLastUpdated / 1000), 'seconds')
-    document.getElementById('last-updated').title = lastUpdatedDate.toLocaleString()
-  }
 }
 
 function renderAlert (alert) {
