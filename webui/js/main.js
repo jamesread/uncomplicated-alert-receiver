1ec2b29752d7d44966987b9a38b7719c0e69d1a1
jamesread <contact@jread.com>
2024-09-01 20:15:40 +0100

    Header renders "critical" if nothing recieved for > 100 seconds

--- webui/js/main.js
+++ webui/js/main.js
@@ -20,11 +20,17 @@ function fetchAlertList () {
 
       if (res.LastUpdated > 0) {
         const lastUpdatedDate = new Date(res.LastUpdated * 1000)
-        const deltaLastUpdated = lastUpdatedDate - new Date()
+        const deltaLastUpdated = Math.floor(lastUpdatedDate - new Date() / 1000)
         const formatter = new Intl.RelativeTimeFormat()
 
-        document.getElementById('last-updated').textContent = formatter.format(Math.floor(deltaLastUpdated / 1000), 'seconds')
+        document.getElementById('last-updated').textContent = formatter.format(deltaLastUpdated, 'seconds')
         document.getElementById('last-updated').title = lastUpdatedDate.toLocaleString()
+
+        if (deltaLastUpdated > 100) {
+          document.getElementById('last-updated').classList.add('critical')
+        } else {
+          document.getElementById('last-updated').classList.remove('critical')
+        }
       }
     })
 }
